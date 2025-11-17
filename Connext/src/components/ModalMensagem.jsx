45f08f6b1212106d3c1d7079b81472fbf4309f29
jopedrofigueiredo <joaopmfigueiredo0209@gmail.com>
2025-11-17 01:00:19 -0300

    Add alert ao enviar mensagem de contato

--- Connext/src/components/ModalMensagem.jsx
+++ Connext/src/components/ModalMensagem.jsx
@@ -1,14 +1,27 @@
 import { DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
-import { Label } from "@radix-ui/react-label"
 import { SendHorizontal } from "lucide-react"
-import { Toggle } from "./ui/toggle"
+import { useState } from "react"
+import AlertSucesso from "./ui/AlertSucesso"
 
 export default function ModalMensagem() {
+    const [showAlert, setShowAlert] = useState(false)
+
+    const handleSubmit = () => {
+      setShowAlert(true)
+    
+      setTimeout(() => {
+        setShowAlert(false)
+      }, 3000)
+    }
 
     return (
           <DialogContent className="sm:max-w-md">
+            {showAlert && (<AlertSucesso titulo={"Sucesso!"} 
+                                         mensagem={"Sua mensagem foi enviada com Sucesso."} 
+                                         className={"absolute z-10 -top-full animate-in transition-all"}
+            />)}
             <DialogHeader>
               <DialogTitle>Entre em contato</DialogTitle>
               <DialogDescription>
@@ -16,18 +29,14 @@ export default function ModalMensagem() {
               </DialogDescription>
             </DialogHeader>
             <div className="flex items-center gap-2">
-              <div className="flex flex-row gap-2 w-full">
-                <Input className=" "/>
-                <Button type="button" variant="secondary">
+              <form className="flex flex-row gap-2 w-full">
+                <Input className=""/>
+                <Button type="button" variant="secondary" onClick={handleSubmit}>
                     <SendHorizontal />
                 </Button>
-              </div>
+              </form>
             </div>
-            <DialogFooter className="sm:justify-start">
-              <DialogClose asChild>
-                
-              </DialogClose>
-            </DialogFooter>
+            <DialogFooter className="sm:justify-start"/>
           </DialogContent>
   )
 }
