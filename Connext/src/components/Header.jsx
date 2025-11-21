10ce28f3fe84b2bb4896a5c9d02bed4520c81c4a
jopedrofigueiredo <joaopmfigueiredo0209@gmail.com>
2025-11-21 10:40:32 -0300

    Add botão toogle theme no Header

--- Connext/src/components/Header.jsx
+++ Connext/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { ToogleTema } from "./ToogleTema";
+
 export default function Header() {
     return (
-        <div className="h-35 flex py-10 justify-center border-b shadow-lg">
-            <h1 className="text-6xl font-medium">conNext</h1>
-        </div>
+        <header className="h-35 flex py-10 justify-center border-b shadow-lg">
+            <div className="max-w-400 w-full flex justify-center relative">
+                <h1 className="text-6xl font-bold text-sky-600">conNext</h1>
+                <div className="absolute right-0 px-2 xs:px-10 h-full flex items-center ">
+                    <ToogleTema />
+                </div>
+            </div>
+        </header>
     )
 }
