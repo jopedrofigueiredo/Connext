163a2c00d1da3beaad65436068c9f1484efebd85
jopedrofigueiredo <joaopmfigueiredo0209@gmail.com>
2025-11-21 10:14:42 -0300

    Importando componente de Tema da tela

--- Connext/src/App.jsx
+++ Connext/src/App.jsx
@@ -1,14 +1,18 @@
 import Header from './components/Header'
 import Feed from './components/Feed'
 import Footer from './components/Footer'
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Feed />
-      <Footer />
+     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Header />
+        <Feed />
+        <Footer />
+      </ThemeProvider>
     </>
   )
 }
